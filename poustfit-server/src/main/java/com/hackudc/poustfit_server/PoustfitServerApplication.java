7058338aa07e222e57a4c10b6c106a153dcaa555
LucasKwak <lkwakiglesia@gmail.com>
2025-02-22 00:37:22 +0100

    [BACK][INITIAL COMMIT] Initial commit of the backend app

---
+++ poustfit-server/src/main/java/com/hackudc/poustfit_server/PoustfitServerApplication.java
@@ -0,0 +1,13 @@
+package com.hackudc.poustfit_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoustfitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoustfitServerApplication.class, args);
+	}
+
+}
---
+++ poustfit-server/src/test/java/com/hackudc/poustfit_server/PoustfitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackudc.poustfit_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoustfitServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
