683eab26f061dd451c662cc34043b30e3af790b6
LucasKwak <lkwakiglesia@gmail.com>
2025-02-22 22:30:38 +0100

    [FRONT][LOGOUT] Now a user logout.

--- poustfit-client/src/features/navigation/components/NavBar.vue
+++ poustfit-client/src/features/navigation/components/NavBar.vue
@@ -1,19 +1,24 @@
 <template>
     <Menubar :model="items">
         <template #item="{ item, props }">
-            <router-link v-slot="{ href, navigate, isActive }" :to="item.route" custom>
+            <router-link v-if="item.route" v-slot="{ href, navigate, isActive }" :to="item.route" custom>
                 <a :href="href" v-bind="props.action" @click="navigate">
                     <span :class="item.icon" />
                     <span :class="{ 'text-bold': isActive }">{{ item.label }}</span>
                 </a>
             </router-link>
+            <Button v-else type="button" :icon="item.icon" :label="item.label" @click="signOut"></Button>
         </template>
     </Menubar>
 </template>
 
 <script setup>
-    import  Menubar from 'primevue/menubar';
+    import { Menubar, Button } from 'primevue';
     import { ref } from 'vue'
+    import { useAuthStore } from '@/stores/authStore.js'
+    import router from '@/router/index.js'
+
+    const authStore = useAuthStore();
 
     const items = ref([
         {
@@ -36,7 +41,20 @@
             icon: 'pi pi-user',
             route: '/my-profile'
         },
+        {
+            label: 'Sign out',
+            icon: 'pi pi-sign-out'
+        }
     ]);
+
+    async function signOut() {
+        try {
+            await authStore.logout();
+            await router.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
 </script>
 
 <style scoped>
